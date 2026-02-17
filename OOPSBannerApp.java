71736554db4de240110cef443ed6fb849b1a6a48
salmanfarismk <sf8990@srmist.edu.in>
2026-02-17 10:06:09 +0530

    Refactored UC2 to use String.join() for efficiency

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,5 +1,13 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        // Use "" as delimiter. Each segment is padded to match the 9-char width requirement.
+        System.out.println(String.join("", " ** ", " *** ", " **** ", " **** "));
+        System.out.println(String.join("", "*  * ", "* * ", "*   * ", "*    "));
+        System.out.println(String.join("", "*  * ", "* * ", "****  ", " *** "));
+        System.out.println(String.join("", "*  * ", "* * ", "*     ", "    * "));
+        System.out.println(String.join("", "*  * ", "* * ", "*     ", "    * "));
+        System.out.println(String.join("", " ** ", " *** ", "*     ", " **** "));
+        
+        System.out.println("\nOOPS"); // Matches the footer in the PDF [cite: 372]
     }
 }
