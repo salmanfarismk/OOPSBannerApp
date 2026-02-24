4327525d55bd32a7f7899c4d892365e7fe268a15
Salman Faris <sf8990@srmist.edu.in>
2026-02-24 08:17:38 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,9 +1,9 @@
 public class OOPSBannerApp {
     public static void main(String[] args) {
-        System.out.println("  *** *** **** **** ");
-        System.out.println(" * * * * * * * ");
-        System.out.println(" * * * * **** **** ");
-        System.out.println(" * * * * * *");
-        System.out.println("  *** *** * **** ");
+        System.out.println(" *** *** **** **** ");
+        System.out.println("  * * * * *   * *   ");
+        System.out.println("  * * * * * **  *** ");
+        System.out.println("  * * * * *        *");
+        System.out.println("  *** *** *     **** ");
     }
 }
